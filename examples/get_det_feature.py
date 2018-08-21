8db4f88629b86b7f824b1ac444895b46ef6d74ca
Yunzhong Hou <hou_yz@outlook.com>
2018-08-21 22:28:04 +0800

    format update

--- examples/get_det_feature.py
+++ examples/get_det_feature.py
@@ -61,7 +61,7 @@ def checkpoint_loader(model, path, eval_only=False):
     return model, start_epoch
 
 
-def extract_features(model, data_loader, print_freq=10):
+def extract_features(model, data_loader, args, print_freq=10):
     model.eval()
     batch_time = AverageMeter()
     data_time = AverageMeter()
@@ -74,7 +74,7 @@ def extract_features(model, data_loader, print_freq=10):
     for i, (imgs, fnames) in enumerate(data_loader):
         data_time.update(time.time() - end)
 
-        outputs = extract_cnn_feature(model, imgs, eval_only=True, output_feature=None)
+        outputs = extract_cnn_feature(model, imgs, eval_only=True, output_feature=args.output_feature)
         for fname, output in zip(fnames, outputs):
             cam, frame = int(fname[1]), int(fname[4:10])
             # f_names[cam - 1].append(fname)
@@ -107,13 +107,12 @@ def main(args):
     if args.height is None or args.width is None:
         args.height, args.width = (256, 128)
 
-    dataset_dir = osp.join(args.data_dir, 'det_dataset_openpose')
+    dataset_dir = osp.join(args.data_dir, 'det_dataset_OpenPose')
     dataset = DetDuke(dataset_dir)
-    normalizer = T.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+    normalizer = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     test_transformer = T.Compose([
-        # T.RectScale(256, 128),
-        T.Resize((256, 128), interpolation=3),
+        T.RectScale(256, 128),
+        # T.Resize((256, 128), interpolation=3),
         T.ToTensor(),
         normalizer,
     ])
@@ -133,16 +132,16 @@ def main(args):
     print('*************** initialization takes time: {:^10.2f} *********************\n'.format(toc))
 
     tic = time.time()
-    lines = extract_features(model, data_loader)
+    lines = extract_features(model, data_loader, args)
     toc = time.time() - tic
     print('*************** compute features takes time: {:^10.2f} *********************\n'.format(toc))
 
-    if not os.path.isdir("det_features_openpose"):
-        os.mkdir("det_features_openpose")
+    if not os.path.isdir("det_features_OpenPose"):
+        os.mkdir("det_features_OpenPose")
         pass
     tic = time.time()
     for cam in range(8):
-        output_fname = 'det_features_openpose/features%d.h5' % (cam + 1)
+        output_fname = 'det_features_OpenPose/features%d.h5' % (cam + 1)
         with h5py.File(output_fname, 'w') as f:
             # asciiList = [n.encode("ascii", "ignore") for n in f_names[cam]]
             # f.create_dataset('f_names', (len(asciiList), 1), 'S10', asciiList)
@@ -171,6 +170,7 @@ if __name__ == '__main__':
     parser.add_argument('--resume', type=str, default='', metavar='PATH')
     parser.add_argument('--features', type=int, default=1024)
     parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--output-feature', type=str, default='None')
     # misc
     parser.add_argument('--seed', type=int, default=1)
     working_dir = osp.dirname(osp.abspath(__file__))
