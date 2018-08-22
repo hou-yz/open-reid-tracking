3e4524a59d5c0f5225003e0d8807dad10e8d79eb
Yunzhong Hou <hou_yz@outlook.com>
2018-08-22 19:43:41 +0800

    Revert "Revert "bad update: temporary""
    This reverts commit c6f359023c66476129c0cc717a90bb842cc9fd9c.

--- reid/evaluators.py
+++ reid/evaluators.py
@@ -10,7 +10,7 @@ from .feature_extraction import extract_cnn_feature
 from .utils.meters import AverageMeter
 
 
-def extract_features(model, data_loader, eval_only, output_feature, print_freq=10, metric=None):
+def extract_features(model, data_loader, eval_only, output_feature, print_freq=10):
     model.eval()
     batch_time = AverageMeter()
     data_time = AverageMeter()
@@ -30,13 +30,13 @@ def extract_features(model, data_loader, eval_only, output_feature, print_freq=1
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if (i + 1) % print_freq == 0:
-            print('Extract Features: [{}/{}]\t'
-                  'Time {:.3f} ({:.3f})\t'
-                  'Data {:.3f} ({:.3f})\t'
-                  .format(i + 1, len(data_loader),
-                          batch_time.val, batch_time.avg,
-                          data_time.val, data_time.avg))
+        # if (i + 1) % print_freq == 0:
+        #     print('Extract Features: [{}/{}]\t'
+        #           'Time {:.3f} ({:.3f})\t'
+        #           'Data {:.3f} ({:.3f})\t'
+        #           .format(i + 1, len(data_loader),
+        #                   batch_time.val, batch_time.avg,
+        #                   data_time.val, data_time.avg))
 
     return features, labels
 
@@ -81,16 +81,16 @@ def evaluate_all(distmat, query=None, gallery=None,
 
     # Compute mean AP
     mAP = mean_ap(distmat, query_ids, gallery_ids, query_cams, gallery_cams)
-    print('Mean AP: {:4.1%}'.format(mAP))
+    # print('Mean AP: {:4.1%}'.format(mAP))
 
     # Compute all kinds of CMC scores
     cmc_configs = {
-        'allshots': dict(separate_camera_set=False,
-                         single_gallery_shot=False,
-                         first_match_break=False),
-        'cuhk03': dict(separate_camera_set=True,
-                       single_gallery_shot=True,
-                       first_match_break=False),
+        # 'allshots': dict(separate_camera_set=False,
+        #                  single_gallery_shot=False,
+        #                  first_match_break=False),
+        # 'cuhk03': dict(separate_camera_set=True,
+        #                single_gallery_shot=True,
+        #                first_match_break=False),
         'market1501': dict(separate_camera_set=False,
                            single_gallery_shot=False,
                            first_match_break=True)}
@@ -98,16 +98,11 @@ def evaluate_all(distmat, query=None, gallery=None,
                             query_cams, gallery_cams, **params)
                   for name, params in cmc_configs.items()}
 
-    print('CMC Scores{:>12}{:>12}{:>12}'
-          .format('allshots', 'cuhk03', 'market1501'))
-    for k in cmc_topk:
-        print('  top-{:<4}{:12.1%}{:12.1%}{:12.1%}'
-              .format(k, cmc_scores['allshots'][k - 1],
-                      cmc_scores['cuhk03'][k - 1],
-                      cmc_scores['market1501'][k - 1]))
+    print('[mAP: {:5.2%}], [cmc1: {:5.2%}], [cmc5: {:5.2%}], [cmc10: {:5.2%}]'
+          .format(mAP, *cmc_scores['market1501'][[0, 4, 9]]))
 
     # Use the allshots cmc top-1 score for validation criterion
-    return cmc_scores['allshots'][0]
+    return cmc_scores['market1501'][0]
 
 
 class Evaluator(object):
--- reid/loss/triplet.py
+++ reid/loss/triplet.py
@@ -5,32 +5,151 @@ from torch import nn
 from torch.autograd import Variable
 
 
+def normalize(x, axis=-1):
+    """Normalizing to unit length along the specified dimension.
+    Args:
+      x: pytorch Variable
+    Returns:
+      x: pytorch Variable, same shape as input
+    """
+    x = 1. * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+    return x
+
+
+def euclidean_dist(x, y):
+    """
+    Args:
+      x: pytorch Variable, with shape [m, d]
+      y: pytorch Variable, with shape [n, d]
+    Returns:
+      dist: pytorch Variable, with shape [m, n]
+    """
+    m, n = x.size(0), y.size(0)
+    xx = torch.pow(x, 2).sum(1, keepdim=True).expand(m, n)
+    yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
+    dist = xx + yy
+    dist.addmm_(1, -2, x, y.t())
+    dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+    return dist
+
+
+def hard_example_mining(dist_mat, labels, return_inds=False):
+    """For each anchor, find the hardest positive and negative sample.
+    Args:
+        dist_mat: pytorch Variable, pair wise distance between samples, shape [N, N]
+        labels: pytorch LongTensor, with shape [N]
+        return_inds: whether to return the indices. Save time if `False`(?)
+    Returns:
+        dist_ap: pytorch Variable, distance(anchor, positive); shape [N]
+        dist_an: pytorch Variable, distance(anchor, negative); shape [N]
+        p_inds: pytorch LongTensor, with shape [N];
+            indices of selected hard positive samples; 0 <= p_inds[i] <= N - 1
+        n_inds: pytorch LongTensor, with shape [N];
+            indices of selected hard negative samples; 0 <= n_inds[i] <= N - 1
+    NOTE: Only consider the case in which all labels have same num of samples,
+        thus we can cope with all anchors in parallel.
+    """
+
+    assert len(dist_mat.size()) == 2
+    assert dist_mat.size(0) == dist_mat.size(1)
+    N = dist_mat.size(0)
+
+    # shape [N, N]
+    is_pos = labels.expand(N, N).eq(labels.expand(N, N).t())
+    is_neg = labels.expand(N, N).ne(labels.expand(N, N).t())
+
+    # `dist_ap` means distance(anchor, positive)
+    # both `dist_ap` and `relative_p_inds` with shape [N, 1]
+    dist_ap, relative_p_inds = torch.max(
+        dist_mat[is_pos].contiguous().view(N, -1), 1, keepdim=True)
+    # `dist_an` means distance(anchor, negative)
+    # both `dist_an` and `relative_n_inds` with shape [N, 1]
+    dist_an, relative_n_inds = torch.min(
+        dist_mat[is_neg].contiguous().view(N, -1), 1, keepdim=True)
+    # shape [N]
+    dist_ap = dist_ap.squeeze(1)
+    dist_an = dist_an.squeeze(1)
+
+    if return_inds:
+        # shape [N, N]
+        ind = (labels.new().resize_as_(labels)
+               .copy_(torch.arange(0, N).long())
+               .unsqueeze(0).expand(N, N))
+        # shape [N, 1]
+        p_inds = torch.gather(
+            ind[is_pos].contiguous().view(N, -1), 1, relative_p_inds.data)
+        n_inds = torch.gather(
+            ind[is_neg].contiguous().view(N, -1), 1, relative_n_inds.data)
+        # shape [N]
+        p_inds = p_inds.squeeze(1)
+        n_inds = n_inds.squeeze(1)
+        return dist_ap, dist_an, p_inds, n_inds
+
+    return dist_ap, dist_an
+
+
 class TripletLoss(nn.Module):
-    def __init__(self, margin=0):
+    def __init__(self, margin=None):
         super(TripletLoss, self).__init__()
         self.margin = margin
-        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        if self.margin is not None:
+            self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        else:
+            self.ranking_loss = nn.SoftMarginLoss()
 
-    def forward(self, inputs, targets):
-        n = inputs.size(0)
-        # Compute pairwise distance, replace by the official when merged
-        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
-        dist = dist + dist.t()
-        dist.addmm_(1, -2, inputs, inputs.t()) # dist = genenalized(input-input.t())^2 = input^2 -2 * input*input.t() +input.t()^2
-        dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
-        # For each anchor, find the hardest positive and negative
-        mask = targets.expand(n, n).eq(targets.expand(n, n).t())
-        dist_ap, dist_an = [], []
-        for i in range(n):
-            dist_ap.append(dist[i][mask[i]].max())
-            dist_an.append(dist[i][mask[i] == 0].min())
-        dist_ap = torch.stack(dist_ap)
-        dist_an = torch.stack(dist_an)
-        # Compute ranking hinge loss
-        y = dist_an.data.new()
-        y.resize_as_(dist_an.data)
-        y.fill_(1)
-        y = Variable(y)
-        loss = self.ranking_loss(dist_an, dist_ap, y)
+    def forward(self, global_feat, labels, normalize_feature=False):
+        """
+            Args:
+                tri_loss: a `TripletLoss` object
+                global_feat: pytorch Variable, shape [N, C]
+                labels: pytorch LongTensor, with shape [N]
+                normalize_feature: whether to normalize feature to unit length along the
+                Channel dimension
+            Returns:
+                loss: pytorch Variable, with shape [1]
+                p_inds: pytorch LongTensor, with shape [N];
+                indices of selected hard positive samples; 0 <= p_inds[i] <= N - 1
+                n_inds: pytorch LongTensor, with shape [N];
+                indices of selected hard negative samples; 0 <= n_inds[i] <= N - 1
+                ==================
+                For Debugging, etc
+                ==================
+                dist_ap: pytorch Variable, distance(anchor, positive); shape [N]
+                dist_an: pytorch Variable, distance(anchor, negative); shape [N]
+                dist_mat: pytorch Variable, pairwise euclidean distance; shape [N, N]
+        """
+        if normalize_feature:
+            global_feat = normalize(global_feat, axis=-1)
+        # shape [N, N]
+        dist_mat = euclidean_dist(global_feat, global_feat)
+        dist_ap, dist_an, p_inds, n_inds = hard_example_mining(dist_mat, labels, return_inds=True)
+
+        y = Variable(dist_an.data.new().resize_as_(dist_an.data).fill_(1))
+        if self.margin is not None:
+            loss = self.ranking_loss(dist_an, dist_ap, y)
+        else:
+            loss = self.ranking_loss(dist_an - dist_ap, y)
         prec = (dist_an.data > dist_ap.data).sum() * 1. / y.size(0)
-        return loss, prec
+
+        # n = inputs.size(0)
+        # # Compute pairwise distance, replace by the official when merged
+        # dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
+        # dist = dist + dist.t()
+        # dist.addmm_(1, -2, inputs, inputs.t())
+        # dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+        # # For each anchor, find the hardest positive and negative
+        # mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+        # dist_ap, dist_an = [], []
+        # for i in range(n):
+        #     dist_ap.append(dist[i][mask[i]].max())
+        #     dist_an.append(dist[i][mask[i] == 0].min())
+        # dist_ap = torch.stack(dist_ap)
+        # dist_an = torch.stack(dist_an)
+        # # Compute ranking hinge loss
+        # y = dist_an.data.new()
+        # y.resize_as_(dist_an.data)
+        # y.fill_(1)
+        # y = Variable(y)
+        # loss = self.ranking_loss(dist_an, dist_ap, y)
+        # prec = (dist_an.data > dist_ap.data).sum() * 1. / y.size(0)
+        return loss, prec, dist_ap, dist_an
