ffbc319008712af737eed7be8b6977aaf69b0628
hou_yz <hou_yz@outlook.com>
2018-09-17 13:45:53 +0800

    Updates: Remove redundant part

--- reid/utils/data/transforms.py
+++ reid/utils/data/transforms.py
@@ -49,36 +49,6 @@ class RandomSizedRectCrop(object):
         return scale(img)
 
 
-class RandomSizedRectCrop(object):
-    def __init__(self, height, width, interpolation=Image.BILINEAR):
-        self.height = height
-        self.width = width
-        self.interpolation = interpolation
-
-    def __call__(self, img):
-        for attempt in range(10):
-            area = img.size[0] * img.size[1]
-            target_area = random.uniform(0.64, 1.0) * area
-            aspect_ratio = random.uniform(2, 3)
-
-            h = int(round(math.sqrt(target_area * aspect_ratio)))
-            w = int(round(math.sqrt(target_area / aspect_ratio)))
-
-            if w <= img.size[0] and h <= img.size[1]:
-                x1 = random.randint(0, img.size[0] - w)
-                y1 = random.randint(0, img.size[1] - h)
-
-                img = img.crop((x1, y1, x1 + w, y1 + h))
-                assert (img.size == (w, h))
-
-                return img.resize((self.width, self.height), self.interpolation)
-
-        # Fallback
-        scale = RectScale(self.height, self.width,
-                          interpolation=self.interpolation)
-        return scale(img)
-
-
 class RandomErasing(object):
     def __init__(self, EPSILON=0.5, mean=[0.485, 0.456, 0.406]):
         self.EPSILON = EPSILON
