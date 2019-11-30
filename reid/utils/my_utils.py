b910c1c0e4fd2e9f181d6f959e87cc59a3d6cec1
Yunzhong Hou <hou_yz@outlook.com>
2019-11-30 23:38:08 +1100

    fix: - curve plotting for eval top1

--- reid/utils/my_utils.py
+++ reid/utils/my_utils.py
@@ -21,7 +21,7 @@ def draw_curve(path, x_epoch, train_loss, train_prec, test_x_epoch=None, test_pr
     ax1 = fig.add_subplot(122, title="prec")
     ax0.plot(x_epoch, train_loss, 'bo-', label='train: {:.3f}'.format(train_loss[-1]))
     ax1.plot(x_epoch, train_prec, 'bo-', label='train: {:.3f}'.format(train_prec[-1]))
-    if test_x_epoch is not None and test_prec is not None:
+    if test_x_epoch and test_prec:
         ax1.plot(test_x_epoch, test_prec, 'ro-', label='test: {:.3f}'.format(test_prec[-1]))
     ax0.legend()
     ax1.legend()
